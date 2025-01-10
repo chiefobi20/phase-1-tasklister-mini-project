0fbf719568108a51057c831e787beb88378fec63
Ahamefule Onyegbule <aonyeobi@gmail.com>
2025-01-10 16:35:53 -0500

    Complete lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+    const createTaskElement = document.getElementById('create-task-form')
+    createTaskElement.addEventListener('submit', (event) => {
+    event.preventDefault()
+
+
+    const newTaskDescriptionElement = document.getElementById('new-task-description')
+
+    const liElement = document.createElement('li')
+    liElement.textContent = newTaskDescriptionElement.value
+
+    const deleteButton = document.createElement('button')
+    deleteButton.textContent = "X"
+    deleteButton.setAttribute('date-description', newTaskDescriptionElement.value)
+    deleteButton.addEventListener('click', (event) => {
+      //liElement.remove()
+      event.target.parentNode.remove()
+    })
+
+    liElement.appendChild(deleteButton)
+
+    const tasksUlElement = document.getElementById('tasks')
+    tasksUlElement.append(liElement)
+
+    createTaskElement.reset()
+  })
 });
